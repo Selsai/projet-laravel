8f187789dac239b30f4c1f9c8a563943cf59d9b7
Amairi <selsabilamairi@gmail.com>
2026-01-12 12:31:41 +0100

    Correction

--- database/migrations/2026_01_12_110811_create_settings_table.php
+++ database/migrations/2026_01_12_110811_create_settings_table.php
@@ -1,22 +1,30 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->unique()->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->string('theme');
             $table->string('lang');
             $table->timestamps();
+            $table->unique('user_id');
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('settings');
     }
